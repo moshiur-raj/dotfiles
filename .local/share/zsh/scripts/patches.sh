036a679c372bbea92a9a2430b38f61c723977034
moshiur <moshiur71199@gmail.com>
2021-04-09 02:48:08 +0600

    moved the fix to mpv cursor size in wayland to patches.sh and mpv-wayland.desktop

--- .local/share/zsh/scripts/patches.sh
+++ .local/share/zsh/scripts/patches.sh
@@ -1,5 +1,5 @@
 # Fix mpv blank screen in gnome wayland
-[[ $WAYLAND_DISPLAY ]] && [[ $XDG_CURRENT_DESKTOP == "GNOME" ]] && alias mpv="gnome-session-inhibit --inhibit idle mpv"
+[[ $WAYLAND_DISPLAY ]] && [[ $XDG_CURRENT_DESKTOP == "GNOME" ]] && alias mpv="env XCURSOR_SIZE=24 gnome-session-inhibit --inhibit idle mpv"
 # Fix alacritty title bar in gnome wayland
 [[ $WAYLAND_DISPLAY ]] && [[ $XDG_CURRENT_DESKTOP == "GNOME" ]] && unset WINIT_UNIX_BACKEND && alias alacritty="env WINIT_UNIX_BACKEND=x11 alacritty"
 # VTE Support in tilix
