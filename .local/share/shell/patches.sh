0988daee6844ed3de66ce54199bcf4a4564bd5d5
moshiur <moshiur71199@gmail.com>
2021-11-20 14:00:36 +0600

    patches.sh: export the variables

--- .local/share/shell/patches.sh
+++ .local/share/shell/patches.sh
@@ -1,3 +1,6 @@
+# Load env vars if systemd failed to do so
+[[ $ENV_CONF_LOADED == "1" ]] || {set -a; source $HOME/.config/environment.d/env.conf; set +a;}
+
 # Fix mpv blank screen in gnome wayland
 [[ $WAYLAND_DISPLAY ]] && [[ $XDG_CURRENT_DESKTOP == "GNOME" ]] && alias mpv="gnome-session-inhibit --inhibit idle mpv --gpu-context=x11egl"
 # Fix alacritty title bar in gnome wayland
