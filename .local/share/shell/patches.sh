30b736521e7b8f67c5e01a0a8ca7676c0d43c028
moshiur <moshiur71199@gmail.com>
2021-11-27 14:48:59 +0600

    patches.sh: use spaces after { and before }

--- .local/share/shell/patches.sh
+++ .local/share/shell/patches.sh
@@ -1,5 +1,5 @@
 # Load env vars if systemd failed to do so
-[[ $ENV_CONF_LOADED == "1" ]] || {set -a; source $HOME/.config/environment.d/env.conf; set +a;}
+[[ $ENV_CONF_LOADED == "1" ]] || { set -a; source $HOME/.config/environment.d/env.conf; set +a; }
 
 # Fix mpv blank screen in gnome wayland
 [[ $WAYLAND_DISPLAY ]] && [[ $XDG_CURRENT_DESKTOP == "GNOME" ]] && alias mpv="gnome-session-inhibit --inhibit idle mpv --gpu-context=x11egl"
