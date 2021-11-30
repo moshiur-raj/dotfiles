091458b7d9c6e8156a1adab25a7c55ae600dd3c9
moshiur <moshiur71199@gmail.com>
2021-11-30 20:47:40 +0600

    init.lua: treesitte - do not highlight current context start

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -352,7 +352,7 @@ require('nvim-treesitter.configs').setup {
 -- Indent-blankline
 require('indent_blankline').setup {
     show_current_context = true,
-    show_current_context_start = true,
+    show_current_context_start = false,
 	show_trailing_blankline_indent = false,
 	strict_tabs = true,
 	char = '┊',
