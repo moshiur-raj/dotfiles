a9a33a966c684a8d9881fd8fa7b677be6b25445e
moshiur <moshiur71199@gmail.com>
2021-11-30 17:27:02 +0600

    init.lua: disable latex highlights from treesitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -343,7 +343,7 @@ require('nvim-treesitter.configs').setup {
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
-    disable = {},  -- list of language that will be disabled
+    disable = {'latex'},  -- list of language that will be disabled
     additional_vim_regex_highlighting = false,
   },
   indent = {enable = true},
