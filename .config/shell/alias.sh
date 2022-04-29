bc64fa497d9c45a34bfbaee939231df4a14c191f
moshiur <moshiur_rahman@tutanota.com>
2022-04-29 17:43:41 +0600

    alias.sh, mpv.conf: (youtube-dl) use av01 codec by default

--- .config/shell/alias.sh
+++ .config/shell/alias.sh
@@ -1,7 +1,8 @@
 # Emacs
 alias emacs="emacsclient -nc"
 # yt-dlp
-alias yt-dlp="yt-dlp --merge-output-format mkv --add-metadata --embed-subs --sub-langs en.\* -f bestvideo\[height\<\=1080\]\[vcodec\=vp9\]\+bestaudio"
+alias yt-dlp='yt-dlp --no-overwrites --merge-output-format mkv --add-metadata --embed-subs --sub-langs "en.*" -f "bestvideo[height<=1080][vcodec^=av01]+bestaudio/bestvideo[height<=1080][vcodec=vp9]+bestaudio/bestvideo[height<=1080]+bestaudio" '
+alias yt-playlist='yt-dlp --no-overwrites --merge-output-format mkv --add-metadata --embed-subs --sub-langs "en.*" -f "bestvideo[height<=1080][vcodec^=av01]+bestaudio/bestvideo[height<=1080][vcodec=vp9]+bestaudio/bestvideo[height<=1080]+bestaudio" -o "%(playlist_index)s-%(title)s.%(ext)s" '
 # doasedit
 alias doasedit='doas env XDG_RUNTIME_DIR_NONROOT=$XDG_RUNTIME_DIR NONROOTUSER=$USERNAME EDITOR=$EDITOR $HOME/.local/bin/doasedit'
 # Managing dotfiles
