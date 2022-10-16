0426b8e6aefc4731711f1085fd4092aebc0873a5
moshiur <moshiur_rahman@tutanota.com>
2022-10-16 21:20:12 +0600

    init.lua: code cleanup and added borders in floating windows

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -118,28 +118,20 @@ nnoremap('<a-t>', '<cmd>rightbelow 15 split +terminal<cr>')
 require('onedark').load()
 
 -- Lualine
-require('lualine').setup({
-  -- extensions = {'nvim-tree'}
-})
+require('lualine').setup()
 
 -- Bufferline
 require('bufferline').setup({
-options = {
-	numbers = 'ordinal',
-	right_mouse_command = nil,
-	diagnostics = 'nvim_lsp',
-	diagnostics_update_in_insert = false,
-	diagnostics_indicator = function(count, level, diagnostics_dict, context)
-		return '('..count..')'
-	end,
-	offsets = {{filetype = 'NvimTree', text = 'File Explorer', text_align = 'left'}},
-	-- do not show terminals in bufferline
-	custom_filter = function(bufn)
-		if not string.match(vim.fn.bufname(bufn), 'term://') then
-			return true
-		end
-	end,
-}
+	options = {
+		numbers = 'ordinal',
+		right_mouse_command = nil,
+		-- do not show terminals in bufferline
+		custom_filter = function(bufn)
+			if not string.match(vim.fn.bufname(bufn), 'term://') then
+				return true
+			end
+		end,
+	}
 })
 -- move between buffers
 nnoremap('<a-j>', '<cmd>BufferLineCycleNext<cr>')
@@ -164,86 +156,85 @@ vim.o.completeopt = 'menu,menuone,noselect'
  -- Setup nvim-cmp.
 local cmp = require('cmp')
 cmp.setup({
- snippet = {
-	 expand = function(args)
-		 require('snippy').expand_snippet(args.body)
-	 end,
- },
- mapping = {
-	 ['<cr>'] = cmp.mapping.confirm({ select = false }),
-	 ['<c-y>'] = cmp.mapping.close(),
-	 ['<tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-	 ['<s-tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
- },
- sources = {
-	 { name = 'snippy', keyword_length = 2 },
-	 { name = 'nvim_lsp' },
-	 { name = 'buffer', keyword_length = 7 },
-	 { name = 'path' },
- }
+	snippet = {
+		expand = function(args)
+			require('snippy').expand_snippet(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = {
+		['<cr>'] = cmp.mapping.confirm({ select = false }),
+		['<c-y>'] = cmp.mapping.close(),
+		['<tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
+		['<s-tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'snippy', keyword_length = 2 },
+		{ name = 'buffer', keyword_length = 5 },
+		{ name = 'path' },
+	}
 })
--- vimtex integration
-local vimtex_augroup = vim.api.nvim_create_augroup('vimtex_augroup', {clear = true})
-autocmd('FileType', { pattern = 'tex', group = vimtex_augroup,
-	callback = function() require('cmp').setup.buffer {
-		sources = {
-			{ name = 'snippy', keyword_length = 2 },
-			{ name = 'nvim_lsp' },
-			{ name = 'omni' },
-			{ name = 'buffer', keyword_length = 7 },
-			{ name = 'path' },
-		}
-	} end,
-})
--- autopairs integration
-cmp.event:on( 'confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done({  map_char = { tex = '' } }))
 
 -- Lspconfig
-local opts = { noremap=true, silent = true }
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+	local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-local servers = {'pyright', 'clangd'}
-for _, lsp in pairs(servers) do
-require('lspconfig')[lsp].setup {
-	on_attach = on_attach,
-	-- cmp integration
-	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-	flags = {
-		debounce_text_changes = 150,
-	}
+local lsp_flags = {
+  debounce_text_changes = 150,
 }
-end
+local handlers =  {
+  ["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'}),
+  ["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'rounded'}),
+}
+require('lspconfig')['pyright'].setup{
+    on_attach = on_attach,
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    flags = lsp_flags,
+	handlers = handlers,
+}
+require('lspconfig')['clangd'].setup{
+    on_attach = on_attach,
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    flags = lsp_flags,
+	handlers = handlers,
+}
 
 -- Treesitter
 require('nvim-treesitter.configs').setup({
 	ensure_installed = {'c', 'python', 'latex', 'bibtex', 'bash', 'lua', 'cpp', 'css', 'html', 'make', 'markdown', 'meson', 'sql', 'json', 'json5'},
 	sync_install = false,
-	ignore_install = {}, -- List of parsers to ignore installing
+	auto_install = true,
 	highlight = {
-		enable = true,              -- false will disable the whole extension
-		disable = {'latex'},  -- list of language that will be disabled
+		enable = true,
+		disable = {'latex'},
 		additional_vim_regex_highlighting = false,
 	},
-	indent = {enable = false},
 })
 
 -- Indent-blankline
@@ -268,6 +259,7 @@ require('nvim-autopairs').setup({
 	disable_filetype = { "TelescopePrompt", "tex" },
 	fast_wrap = {},
 })
+cmp.event:on( 'confirm_done', require('nvim-autopairs.completion.cmp').on_confirm_done() )
 
 -- Snippy
 require('snippy').setup({
@@ -279,7 +271,7 @@ require('snippy').setup({
 	},
 })
 
--- Lightspeed
+-- Leap
 require('leap').set_default_keymaps()
 
 -- Neoscroll
@@ -288,5 +280,24 @@ require('neoscroll').setup()
 -- Vimtex
 vim.g.tex_flavor = 'latex'
 
--- Autotag
-require('nvim-ts-autotag').setup()
+local vimtex_augroup = vim.api.nvim_create_augroup('vimtex_augroup', {clear = true})
+autocmd('FileType', { pattern = 'tex', group = vimtex_augroup,
+callback = function() require('cmp').setup.buffer {
+	formatting = {
+		format = function(entry, vim_item)
+			vim_item.menu = ({
+				omni = (vim.inspect(vim_item.menu):gsub('%"', "")),
+				buffer = "[Buffer]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
+	sources = {
+		{ name = 'snippy', keyword_length = 2 },
+		{ name = 'nvim_lsp' },
+		{ name = 'omni' },
+		{ name = 'buffer', keyword_length = 5 },
+		{ name = 'path' },
+	}
+} end,
+})
