feb11d858bcd3649b29706532f03c3ef1827942f
moshiur <moshiur_rahman@tutanota.com>
2024-10-11 01:57:55 +0600

    reworked fuzzy completion, now buffer is modified when selecting files to edit

--- .config/shell/fuzzy-completion.sh
+++ .config/shell/fuzzy-completion.sh
@@ -1,7 +1,8 @@
 # quick edit
 source /usr/share/fzf/completion.zsh
 source /usr/share/fzf/key-bindings.zsh
-FUZZY_FINDER=fzf
+
+FUZZY_FINDER=("fzf" "--border" "--height" "60%")
 
 CONFIG_FILES=(~/.local/bin $XDG_CONFIG_HOME/{bashrc,nvim,zsh,mpv,alacritty,npm,environment.d,shell} $XDG_DATA_HOME/applications ~/Nextcloud/Notes ~/coding ~/todo ~/notes ~/research ~/Templates)
 
@@ -9,13 +10,25 @@ find_files() {
 	find ${CONFIG_FILES[@]} \( -path "*/.cache" -o -path "*/.clangd" -o -path "*/.git" -o -path "*.pdf" -o -path "*.pdf_tex" -o -path "*.mkv" -o -path "*.svg" -o -path "*.glsl" -o -path "*.png" -o -path "*.jpg" \) -prune -o -type f -print
 }
 # edit config files
-ef() {
-	find_files | $FUZZY_FINDER | xargs -r -d "\n" $EDITOR
+edit_config_file() {
+	local file
+	file=$(find_files | $FUZZY_FINDER)
+	zle redisplay
+
+	if [ -n "$file" ]; then
+		BUFFER="$EDITOR \"$file\""
+		zle accept-line
+	else
+		echo "No file selected."
+	fi
 }
+zle -N edit_config_file
+bindkey "^e" edit_config_file
 
 # fuzzy completion
 fuzzy_completion() {
 	LBUFFER="${LBUFFER}$(find_files | $FUZZY_FINDER | xargs -d "\n" printf "%q" )"
+	zle redisplay
 }
 
 zle -N fuzzy_completion
