6689e2850d64cb9a238e22d5ead33457aee6b613
moshiur <moshiur_rahman@tutanota.com>
2025-05-27 17:40:28 +0600

    modified:   .config/mpv/scripts/yt-dlp.lua

--- .config/mpv/scripts/yt-dlp.lua
+++ .config/mpv/scripts/yt-dlp.lua
@@ -3,6 +3,7 @@ local utils = require 'mp.utils'
 
 -- Define quality options (modify as needed)
 local quality_options = {
+    {label = "1080p", format = "bestvideo[height<=1080][vcodec^=av01]+bestaudio/bestvideo[height<=1080][vcodec=vp9]+bestaudio/bestvideo[height<=1080]+bestaudio"},
     {label = "1440p", format = "bestvideo[height<=1440][vcodec^=av01]+bestaudio/bestvideo[height<=1440][vcodec=vp9]+bestaudio/bestvideo[height<=1440]+bestaudio"},
     {label = "2160p", format = "bestvideo[height<=2160][vcodec^=av01]+bestaudio/bestvideo[height<=2160][vcodec=vp9]+bestaudio/bestvideo[height<=2160]+bestaudio"},
     {label = "Best", format = "bestaudio+bestvideo"},
@@ -11,6 +12,8 @@ local quality_options = {
 local current_quality_index = 1  -- Start with the first quality option
 
 local function cycle_quality()
+    -- Move to the next quality, loop back if at the end
+    current_quality_index = (current_quality_index % #quality_options) + 1
     local choice = quality_options[current_quality_index]
 
     -- Set the new quality
@@ -20,11 +23,7 @@ local function cycle_quality()
     -- Reload video with the new quality setting
     local path = mp.get_property("path")
     mp.commandv("loadfile", path, "replace")
-
-    -- Move to the next quality, loop back if at the end
-    current_quality_index = (current_quality_index % #quality_options) + 1
 end
 
 -- Bind "y" key to cycle through qualities
 mp.add_key_binding("y", cycle_quality)
-
