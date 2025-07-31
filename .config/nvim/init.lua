a02b008fda505aaf793182d3fea37c59c7e2ce7e
moshiur <moshiur_rahman@tutanota.com>
2025-07-31 16:25:45 +0600

    add warning system when plugins are not updated for more than a week

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,30 @@ vim.opt.rtp:prepend(lazypath)
 require('plugins')
 
 
+-- Check if plugins are outdated
+local function warn_if_plugins_outdated()
+	local lockfile = os.getenv("XDG_CONFIG_HOME") or (os.getenv("HOME") .. "/.config")
+	lockfile = lockfile .. "/nvim/lazy-lock.json"
+
+	local stat = vim.loop.fs_stat(lockfile)
+	if not stat then
+		vim.notify("Warning! lazy-lock.json not found", vim.log.levels.WARN)
+		return
+	end
+
+	local last_modified = stat.mtime.sec
+	local current_time = os.time()
+	local one_week = 7 * 24 * 60 * 60
+
+	if current_time - last_modified > one_week then
+		vim.notify("Warning! You have not updated your plugins in at least a week", vim.log.levels.WARN)
+	end
+end
+
+-- Call the function on startup
+warn_if_plugins_outdated()
+
+
 --------------------
 -- Vim Options
 --------------------
@@ -291,7 +315,8 @@ npairs.add_rules({
 })
 
 -- Leap
-require('leap').set_default_mappings()
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
 
 -- Neoscroll
 require('neoscroll').setup()
